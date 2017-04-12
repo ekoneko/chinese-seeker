cbcd89b6757ee00a6cd5e620c7d7b900888191ca
ekoneko <9@ikaze.me>
2017-04-13 01:09:53 +0800

    init

---
+++ config.sample.js
@@ -0,0 +1,5 @@
+module.exports = {
+    projectPath: '/path/to/project',
+    ignore: ['node_modules/**', 'locale/**'],
+    scope: /[^\x00-\xff]/g,
+}
---
+++ index.js
@@ -0,0 +1,14 @@
+const config = require('./config')
+const scanFiles = require('./lib/scanFiles');
+const parse = require('./lib/parse');
+const resultFormat = require('./lib/resultFormat');
+
+scanFiles(config.projectPath, config.ignore).then(files => {
+  return Promise.all(files.map(file => {
+    return parse(file, config.scope);
+  }))
+}).then(res => {
+  resultFormat(res, config.projectPath);
+}).catch(err => {
+  console.error(err)
+})
